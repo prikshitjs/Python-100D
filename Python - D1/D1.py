64dbe0851fe5e8bb45441fc548ad6894b590ca1b
Prikshit js <prikshitxcode@gmail.com>
2025-02-22 12:47:23 +0530

    Add files via upload

---
+++ Python - D1/D1.py
@@ -0,0 +1,54 @@
+# variables in python and how we show our stored data in variable
+
+name = "Prikshit";
+
+# in this line name is a variable name and "=" is a asignment operator you can store your
+# string in single or double quote
+
+# integer store in python
+# now we store age in a variable in python name with "age"
+
+age = 16;
+
+# you cannot store a integer in duble or single quote if you store then python give you error
+
+#and this is how you can store any datatype in variable like - strings, int, float, bool
+
+store_string  = "your name"; #enter your name
+store_int     = 97; #store the numbers
+store_float   = 9.75;
+store_boolean = bool(1) #store true or false
+
+'''
+
+now if you want to show your data to user then you use print function
+print is a inbuild function of python thst used to show text on user screen
+the syntax of print function is print(variable/"strint"/int/float/bool);
+
+'''
+
+print(name);
+print(age);
+
+# if you follow the above code then your output is-
+
+'''
+Prikshit
+16
+'''
+
+# and this is how you can print some datatypes
+
+print(store_string);
+print(store_int);
+print(store_float);
+print(store_boolean);
+
+# when you run this then your output is -
+
+'''
+your name - store_string
+97        - store_int
+9.75      - store_float
+True      - store_boolean
+'''
