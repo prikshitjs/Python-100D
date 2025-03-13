6e42c84d22962df498a27b8d2e7c73f5bd64d551
Prikshit js <prikshitxcode@gmail.com>
2025-03-13 12:44:56 +0530

    learn type casting in python
    learn how to specify a variable type

---
+++ Python - D5/D5.py
@@ -0,0 +1,55 @@
+# type casting in python
+# to specify the variable type
+
+# for eg we have a float number 1.20 and i want convert this into integer
+# first create a float variable
+
+float_num = 1.20;
+# now we use int() function to convert float no into int
+
+int_num = int(float_num)
+print(type(int_num));
+
+# other example
+
+'''
+int(a, base)
+	
+
+Converts any data type into an integer. Here, base is an optional parameter specifying the base of the string. 
+
+float()
+	
+
+Converts a data type into a floating-point number.
+
+ord()
+	
+
+Converts a character into an integer.
+
+hex()
+	
+
+Converts an integer into a hexadecimal string.
+
+oct()
+	
+
+Converts an integer into an octal string.
+
+tuple()
+	
+
+Converts a data type into a tuple.
+
+list()
+	
+
+Converts a data type into a list.
+
+set()
+	
+
+Converts a data type into a set.
+'''
